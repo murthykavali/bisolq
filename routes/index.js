0c257b3109372df4fdb071dad0ec095db5f776d2
gs-mkavali <mkavali@gainsight.com>
2015-04-14 08:16:22 +0530

    first commit - working project

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/home', function (req, res, next) {
+    res.render('home', {title: 'Home'});
+});
+
+/* Frameworks */
+router.get('/frameworks', function (req, res, next) {
+    res.render('index', {title: 'Frameworks'});
+});
+
+/* SMAC */
+router.get('/smac', function (req, res, next) {
+    res.render('index', {title: 'SMAC'});
+});
+
+/* Domains */
+router.get('/domains', function (req, res, next) {
+    res.render('index', {title: 'Domains'});
+});
+
+/* Team */
+router.get('/team', function (req, res, next) {
+    res.render('index', {title: 'Team'});
+});
+
+/* Contact Us */
+router.get('/contactus', function (req, res, next) {
+    res.render('index', {title: 'Contact Us'});
+});
+
+/* Carriers */
+router.get('/careers', function (req, res, next) {
+    res.render('index', {title: 'Careers'});
+});
+
+/* Services */
+router.get('/services', function (req, res, next) {
+    res.render('index', {title: 'Services'});
+});
+
+router.get('/services/test-and-quality', function (req, res, next) {
+    res.render('test-and-quality', {title: 'Test and Quality Engineering'});
+});
+
+router.get('/services/packaged-applications', function (req, res, next) {
+    res.render('packaged-applications', {title: 'Packaged Applications'});
+});
+
+router.get('/services/onsite-or-offshore', function (req, res, next) {
+    res.render('onsite-or-offshore', {title: 'Onsite or Offshore Trainings'});
+});
+
+module.exports = router;
